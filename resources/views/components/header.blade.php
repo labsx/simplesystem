6c05976ff36977ca3c9ce8a888cb480905e2edd7
labsx <jrlabrador@revlv.com>
2023-08-17 19:41:00 -0400

    Doing some simple system with css

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,21 @@
+
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    
+        <link rel="stylesheet" href="css/style.css">
+    
+        <link rel="icon" href="Favicon.png">
+    
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    
+        <title>Laravel</title>
+    </head>
